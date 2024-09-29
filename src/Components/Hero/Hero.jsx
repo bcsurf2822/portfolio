e15ea1f8a665e107cc08523eb15e8b1d0e2534e9
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-09-29 14:38:25 -0400

    have new website coming together title navbar and information is in there

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <img src={profile_img} alt='' />
+      <h1><span>Ben Corbett  </span>Full-Stack Developer  NJ, USA</h1>
+      <p>Im a full stack developer from Marmora, NJ.  I recently completed Parsity, a full stack coding bootcamp</p>
+      <div className='hero-action'></div>
+      <div className="hero-connect">Connect with me</div>
+      <div className="hero-resume">Resume</div>
+    </div>
+  )
+}
+
+export default Hero
